573d3ccdcce5eea8e3d49d7099a60b2bbb09a4e3
Danny Kirchmeier <danny@kirchmeier.us>
2019-12-02 09:00:27 -0600

    Day 01

--- 01/index.ts
+++ 01/index.ts
@@ -5,19 +5,39 @@ import _ = require('lodash');
 function main() {
   const input = _.trim(readFileSync(__dirname + '/input.txt', 'utf8'));
 
-  assert.strictEqual(null, solveA(""));
-  assert.strictEqual(null, solveA(input));
+  assert.strictEqual(654, solveA("1969"));
+  assert.strictEqual(33583, solveA("100756"));
+  assert.strictEqual(3252897, solveA(input));
 
-  assert.strictEqual(null, solveB(""));
-  assert.strictEqual(null, solveB(input));
+  assert.strictEqual(2, solveB("14"));
+  assert.strictEqual(966, solveB("1969"));
+  assert.strictEqual(50346, solveB("100756"));
+  assert.strictEqual(4876469, solveB(input));
 }
 
 main();
 
 function solveA(input: string) {
-  return input.length;
+  let sum = 0;
+  for (let n of input.split('\n').map(Number)) {
+    sum += calcFuel(n);
+  }
+  return sum;
 }
 
+
 function solveB(input: string) {
-  return input.length;
+  let sum = 0;
+  for (let n of input.split('\n').map(Number)) {
+    let lastFuel = calcFuel(n);
+    while (lastFuel >= 0) {
+      sum += lastFuel;
+      lastFuel = calcFuel(lastFuel);
+    }
+  }
+  return sum;
+}
+
+function calcFuel(mass: number) {
+  return Math.floor(mass / 3) - 2;
 }
