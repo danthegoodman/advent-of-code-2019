26ac501364538900be62347abb973c23c1451aeb
Danny Kirchmeier <danny@kirchmeier.us>
2020-06-30 08:43:46 -0500

    Day 07 [part 2]

--- 07/index.ts
+++ 07/index.ts
@@ -10,24 +10,26 @@ function main() {
   assert.strictEqual(65210, solveA("3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"));
   assert.strictEqual(13848, solveA(input));
 
-  assert.strictEqual(null, solveB(""));
+  assert.strictEqual(139629729, solveB("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"));
   assert.strictEqual(null, solveB(input));
 }
 
 main();
 
 function solveA(input: string) {
-  let originalProgram = input.split(',').map(Number);
+  const originalProgram = input.split(',').map(Number);
 
   let maxOutput = -1;
-  for (let inputs of permutations([0, 1, 2, 3, 4])) {
+  for (const inputs of permutations([0, 1, 2, 3, 4])) {
     let lastOutput = 0;
-    for(let i of inputs){
-      let inputFetchCount = 0;
-      runIntcodeProgram(Array.from(originalProgram), ()=> {
-        inputFetchCount += 1;
-        return inputFetchCount === 1 ? i : lastOutput;
-      }, (n: number) => lastOutput = n)
+    for(const i of inputs){
+      const input = [i, lastOutput];
+      const gen = runIntcodeProgram({
+        program: Array.from(originalProgram),
+        input: input.values(),
+      })
+      lastOutput = gen.next().value;
+      gen.return(null);
     }
     if(maxOutput < lastOutput){
       maxOutput = lastOutput;
@@ -38,7 +40,24 @@ function solveA(input: string) {
 }
 
 function solveB(input: string) {
-  return input.length;
+  const originalProgram = input.split(',').map(Number);
+
+  let maxOutput = -1;
+  for (const inputs of permutations([5,6,7,8,9])) {
+    const inputA = [inputs[0], 0];
+    const genA = runIntcodeProgram({program: Array.from(originalProgram), input: inputA.values()})
+    const genB = runIntcodeProgram({program: Array.from(originalProgram), input: prefixedIterator([inputs[1]], genA)})
+    const genC = runIntcodeProgram({program: Array.from(originalProgram), input: prefixedIterator([inputs[2]], genB)})
+    const genD = runIntcodeProgram({program: Array.from(originalProgram), input: prefixedIterator([inputs[3]], genC)})
+    const genE = runIntcodeProgram({program: Array.from(originalProgram), input: prefixedIterator([inputs[4]], genD)})
+
+    for(let value of genE){
+      inputA.push(value);
+    }
+    maxOutput = Math.max(inputA[inputA.length - 1], maxOutput)
+  }
+
+  return maxOutput;
 }
 
 function* permutations(options: number[]): Generator<number[]> {
@@ -56,7 +75,14 @@ function* permutations(options: number[]): Generator<number[]> {
   }
 }
 
-function runIntcodeProgram(program: number[], getInput: () => number, pushOutput: (n: number) => void) {
+function* prefixedIterator(values: number[], iterator: IterableIterator<number>){
+  yield* values;
+  yield* iterator;
+}
+
+function* runIntcodeProgram(options: {program: number[], input: Iterator<number>}): Generator<number> {
+  const {program, input} = options;
+
   let ndx = 0;
   while (true) {
     let [, cM, bM, aM, code] = program[ndx].toString().padStart(5, '0').match(/(.)(.)(.)(..)/)!;
@@ -78,12 +104,12 @@ function runIntcodeProgram(program: number[], getInput: () => number, pushOutput
       continue;
     }
     if (code === '03') {
-      program[aP] = getInput();
+      program[aP] = input.next().value;
       ndx += 2;
       continue;
     }
     if (code === '04') {
-      pushOutput(aV);
+      yield aV;
       ndx += 2;
       continue;
     }
